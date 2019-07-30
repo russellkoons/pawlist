f4eb1c8cb7ea81b98a4b1120ca00313b1978b229
russellkoons <rkoons@gmail.com>
2019-07-30 17:09:21 -0400

    Nav component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function Nav() {
-
+export class Nav extends React.Component {
+  render() {
+    return(
+      <nav>
+        <h1>Pawlist</h1>
+        <a href="_blank">Pets</a>
+        <a href="_blank">Calendar</a>
+      </nav>
+    )
+  }
 }
-
-export default Nav;
