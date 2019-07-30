abda2b5b6262fceef170252df184c46522090668
russellkoons <rkoons@gmail.com>
2019-07-30 17:15:10 -0400

    Splash Component

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Splash() {
-
+export class Splash extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Welcome to Pawlist</h1>
+        <p>As pet lovers we all struggle with keeping track of feeding times, playdates, and vet visits, but with Pawlist all this info in available in one place! Save your vet info and vaccination dates and we'll remind you on our Calendar page when they're due!</p>
+      </div>
+    )
+  }
 }
-
-export default Splash;
