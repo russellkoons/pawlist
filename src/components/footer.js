106286b644e0b620f928a96b416cc3e73a3f2680
russellkoons <rkoons@gmail.com>
2019-08-05 17:05:19 -0400

    footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+        <p>Created and coded by Russell Koons &copy;2019</p>
+      </footer>
+    )
+  }
+}
