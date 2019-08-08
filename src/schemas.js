1358fed1d61ef6f22d513ec671b46902f5a5b07b
russellkoons <rkoons@gmail.com>
2019-08-08 16:59:27 -0400

    signup schema

--- src/schemas.js
+++ src/schemas.js
@@ -1,8 +1,19 @@
 import * as Yup from 'yup';
 
-export cost LoginSchema = Yup.object().shape({
+export const LoginSchema = Yup.object().shape({
   username: Yup.string()
     .required('Required'),
   password: Yup.string()
+    .required('Required')
+});
+
+export const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Username too short!')
+    .max(18, 'Username too long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'Password too short!')
+    .max(72, 'Password too long!')
     .required('Required')
 });
