b27dc292b26bff109aec05be3bcf7dc5f04d0d29
russellkoons <rkoons@gmail.com>
2019-08-08 17:19:56 -0400

    started event form

--- src/components/eventform.js
+++ src/components/eventform.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {Formik, Form, FieldArray, ErrorMessage, Field} from 'formik';
+import {EventSchema} from '../schemas';
 
 export default class EventForm extends React.Component {
+  handleSubmit = (values, actions) => {
+    console.log(values);
+  }
+
   render() {
     return(
-
+      <div>
+        <h2>New Event</h2>
+        <Formik 
+          id="new-event"
+          initialValues={{
+            name: '',
+            date: '',
+            pets: [''],
+            info: {},
+            frequency: ''
+          }}
+          validationSchema={EventSchema}
+          onSubmit={(values, actions) => this.handleSubmit(values, actions)}
+          render={({
+            values,
+            isSubmitting
+          }) => (
+            <Form>
+              
+            </Form>
+          )}
+        />
+      </div>
     )
   }
 }
