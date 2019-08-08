f7a211a401ccf46650fb25c190e00a8715aa2b51
russellkoons <rkoons@gmail.com>
2019-08-08 17:14:12 -0400

    pet schema

--- src/schemas.js
+++ src/schemas.js
@@ -22,5 +22,26 @@ export const EventSchema = Yup.object().shape({
   name: Yup.string().required('Required'),
   date: Yup.string().required('Required'),
   pets: Yup.array(),
-  description: Yup.string()
+  info: Yup.object().shape({
+    location: Yup.string(),
+    description: Yup.string()
+  }),
+  frequency: Yup.string()
+});
+
+export const PetSchema = Yup.object().shape({
+  name: Yup.string().required('Required'),
+  info: Yup.object().shape({
+    birthday: Yup.string(),
+    breed: Yup.string(),
+    color: Yup.string(),
+    weight: Yup.string()
+  }),
+  vet: Yup.object().shape({
+    vetName: Yup.string(),
+    phone: Yup.string(),
+    shots: Yup.object(),
+    visits: Yup.array()
+  }),
+  pic: Yup.string()
 });
